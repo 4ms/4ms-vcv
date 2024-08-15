1f8d9b4872bef2b826d8863d07012aaf29dde9c0
Dan Green <danngreen1@gmail.com>
2024-08-14 22:32:22 -0700

    Remove unused file

--- src/scaling_config.hh
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-static inline constexpr float InputLowRangeVolts = -5.f;
-static inline constexpr float InputHighRangeVolts = 5.f;
-
-static inline constexpr float OutputLowRangeVolts = -5.f;
-static inline constexpr float OutputHighRangeVolts = 5.f;
