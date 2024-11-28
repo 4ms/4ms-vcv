2cb8d3083a70e0609ce2d616bfe46b084d6e5e87
Dan Green <danngreen1@gmail.com>
2024-11-27 20:43:57 -0800

    DefaultValue=>default_value (needed for CoreModules update to v2.0-dev)

--- src/widgets/base_modules_implemenation.hh
+++ src/widgets/base_modules_implemenation.hh
@@ -15,7 +15,7 @@ inline void do_config_element(JackOutput el, const Indices &indices, const Modul
 };
 
 inline void do_config_element(Pot el, const Indices &indices, const ModuleContext_t &context) {
-	context.module->configParam(indices.param_idx, 0.f, 1.f, el.DefaultValue, el.short_name.data());
+	context.module->configParam(indices.param_idx, 0.f, 1.f, el.default_value, el.short_name.data());
 };
 
 inline void do_config_element(LightElement el, const Indices &indices, const ModuleContext_t &context) {
@@ -27,19 +27,19 @@ inline void do_config_element(MomentaryButton el, const Indices &indices, const 
 }
 
 inline void do_config_element(LatchingButton el, const Indices &indices, const ModuleContext_t &context) {
-	float defaultValue = (el.DefaultValue == LatchingButton::State_t::UP) ? 0 : 1;
+	float defaultValue = (el.default_value == LatchingButton::State_t::UP) ? 0 : 1;
 	context.module->configParam(indices.param_idx, 0, 1, defaultValue, el.short_name.data());
 }
 
 inline void do_config_element(SlideSwitch el, const Indices &indices, const ModuleContext_t &context) {
-	context.module->configParam(indices.param_idx, 0, el.num_pos - 1, el.DefaultValue, el.short_name.data());
+	context.module->configParam(indices.param_idx, 0, el.num_pos - 1, el.default_value, el.short_name.data());
 };
 
 inline void do_config_element(FlipSwitch el, const Indices &indices, const ModuleContext_t &context) {
 	std::vector<std::string> labels;
 	for (auto p : el.pos_names)
 		labels.push_back(std::string{p});
-	context.module->configSwitch(indices.param_idx, 0, el.num_pos - 1, el.DefaultValue, el.short_name.data(), labels);
+	context.module->configSwitch(indices.param_idx, 0, el.num_pos - 1, el.default_value, el.short_name.data(), labels);
 };
 
 inline void do_config_element(Encoder el, const Indices &indices, const ModuleContext_t &context) {
@@ -47,11 +47,11 @@ inline void do_config_element(Encoder el, const Indices &indices, const ModuleCo
 };
 
 inline void do_config_element(AltParamContinuous el, const Indices &indices, const ModuleContext_t &context) {
-	context.module->configParam(indices.param_idx, el.MinValue, el.MaxValue, el.DefaultValue, el.short_name.data());
+	context.module->configParam(indices.param_idx, el.MinValue, el.MaxValue, el.default_value, el.short_name.data());
 }
 
 inline void do_config_element(AltParamChoice el, const Indices &indices, const ModuleContext_t &context) {
-	context.module->configParam(indices.param_idx, 0, el.num_pos - 1, el.DefaultValue, el.short_name.data());
+	context.module->configParam(indices.param_idx, 0, el.num_pos - 1, el.default_value, el.short_name.data());
 }
 
 } // namespace MetaModule::VCVImplementation::Module
