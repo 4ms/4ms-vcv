4e98689b00409554a4edbac6db03ddd588589f0f
audiobird <cantrellhepner@gmail.com>
2025-02-11 16:43:10 -0800

    Add ui and yml backend for custom panel jack names
    .
    
    .

---
+++ src/hub/jack_alias.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace MetaModule
+{
+
+struct JackAlias {
+	std::vector<std::string> in;
+	std::vector<std::string> out;
+};
+
+} // namespace MetaModule
