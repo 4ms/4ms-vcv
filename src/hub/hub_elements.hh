6ff70098ea41691b4f241ce84bfed0427c97cc5f
audiobird <cantrellhepner@gmail.com>
2025-02-12 13:43:43 -0800

    Add option to change alias in right click menu

--- src/hub/hub_elements.hh
+++ src/hub/hub_elements.hh
@@ -3,6 +3,7 @@
 #include "CoreModules/elements/elements.hh"
 #include "console/pr_dbg.hh"
 #include "hub/hub_module_widget.hh"
+#include "hub/jack_alias_menu.hh"
 #include "widgets/4ms/4ms_widgets.hh"
 
 namespace MetaModule
@@ -28,14 +29,31 @@ inline void do_create(Knob el, const ElementCount::Indices &idx, const HubWidget
 		ctx.module_widget->addLabeledKnobPx<Small9mmKnob>(el.short_name, idx.param_idx, ctr_pos, 14.f);
 }
 
+struct HubPort : rack::PJ301MPort {
+	std::string *alias;
+
+	void appendContextMenu(rack::ui::Menu *menu) override {
+		using namespace rack;
+		menu->addChild(new MenuSeparator());
+		menu->addChild(new JackNameMenuItem{[this](unsigned _, std::string const &text) { *alias = text; },
+											{},
+											"alias",
+											*alias,
+											MetaModuleHubWidget::kMaxJackAliasChars});
+		menu->addChild(new MenuSeparator());
+	}
+};
+
 inline void do_create(JackInput el, const ElementCount::Indices &idx, const HubWidgetContext &ctx) {
-	ctx.module_widget->addInput(
-		rack::createInputCentered<rack::PJ301MPort>(rack::mm2px({el.x_mm, el.y_mm}), ctx.module, idx.input_idx));
+	auto p = rack::createInputCentered<HubPort>(rack::mm2px({el.x_mm, el.y_mm}), ctx.module, idx.input_idx);
+	p->alias = &ctx.module_widget->hubModule->jack_alias.out[idx.input_idx];
+	ctx.module_widget->addInput(p);
 }
 
 inline void do_create(JackOutput el, const ElementCount::Indices &idx, const HubWidgetContext &ctx) {
-	ctx.module_widget->addOutput(
-		rack::createOutputCentered<rack::PJ301MPort>(rack::mm2px({el.x_mm, el.y_mm}), ctx.module, idx.output_idx));
+	auto p = rack::createOutputCentered<HubPort>(rack::mm2px({el.x_mm, el.y_mm}), ctx.module, idx.output_idx);
+	p->alias = &ctx.module_widget->hubModule->jack_alias.in[idx.output_idx];
+	ctx.module_widget->addOutput(p);
 }
 
 inline void do_create(LatchingButton el, const ElementCount::Indices &idx, const HubWidgetContext &ctx) {
