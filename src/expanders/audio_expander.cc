4103b2cf3be8c69c7e86ee2769ea89f373429701
audiobird <cantrellhepner@gmail.com>
2025-02-11 16:43:10 -0800

    Add ui and yml backend for custom panel jack names

--- src/expanders/audio_expander.cc
+++ src/expanders/audio_expander.cc
@@ -1,6 +1,8 @@
 #include "CoreModules/elements/element_counter.hh"
 #include "CoreModules/hub/MMAudioExpander_info.hh"
 #include "hub/hub_elements.hh"
+#include "hub/jack_alias.hh"
+#include "hub/jack_alias_menu.hh"
 #include "widgets/4ms/4ms_widgets.hh"
 #include "widgets/config_element.hh"
 
@@ -13,6 +15,10 @@ struct AudioExpanderModule : MetaModuleHubBase {
 		// Register with VCV the number of elements of each type
 		auto cnt = ElementCount::count<Info>();
 		config(cnt.num_params, cnt.num_inputs, cnt.num_outputs, cnt.num_lights);
+
+		// hardware io is virtually inverted
+		jack_alias.in.resize(cnt.num_outputs);
+		jack_alias.out.resize(cnt.num_inputs);
 
 		// Configure elements with VCV
 		ConfigElement<Info> creator{this};
@@ -28,10 +34,11 @@ struct AudioExpanderModule : MetaModuleHubBase {
 struct AudioExpanderWidget : MetaModuleHubWidget {
 	using Info = AudioExpanderModule::Info;
 
-	AudioExpanderWidget(rack::Module *module) {
+	AudioExpanderWidget(AudioExpanderModule *module) {
 		using namespace rack;
 
 		setModule(module);
+		hubModule = module;
 		setPanel(APP->window->loadSvg(rack::asset::plugin(pluginInstance, std::string{Info::svg_filename})));
 		addChild(createWidget<ScrewBlack>(rack::math::Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewBlack>(rack::math::Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
@@ -40,6 +47,14 @@ struct AudioExpanderWidget : MetaModuleHubWidget {
 		for (auto &element : Info::Elements) {
 			std::visit([&creator](auto &el) { creator.create(el); }, element);
 		}
+	}
+
+	void appendContextMenu(rack::Menu *menu) override {
+		using namespace rack;
+		menu->addChild(new MenuSeparator());
+		auto jacks = std::array{std::make_tuple(std::span{hubModule->jack_alias.in}, "Input "),
+								std::make_tuple(std::span{hubModule->jack_alias.out}, "Output ")};
+		menu->addChild(createAliasSubmenu(jacks));
 	}
 };
 
