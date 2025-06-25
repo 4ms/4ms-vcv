ab8975755cb1b0c1b8c0077691c48f3929fe9374
Dan Green <danngreen1@gmail.com>
2025-06-24 22:39:30 -0700

    Find button expander modules in patch

--- src/mapping/expanders.hh
+++ src/mapping/expanders.hh
@@ -5,17 +5,21 @@
 #include "mapping/module_directory.hh"
 #include <cstdint>
 #include <rack.hpp>
+#include <vector>
 
 namespace MetaModule
 {
 
 struct ExpanderMappings {
 private:
-	struct Audio {
-		int64_t module_id = -1;
-	};
+	int64_t audio_exp_module_id = -1;
 
-	Audio audio_exp{};
+	constexpr static size_t MaxButtonExpanders = 4;
+	struct ButtonExp {
+		int64_t module_id = -1;
+		// MetaModuleHubBase *module = nullptr;
+	};
+	std::vector<ButtonExp> button_exps{};
 
 public:
 	void addModule(rack::Module *module) {
@@ -23,40 +27,46 @@ public:
 			return;
 
 		if (ModuleDirectory::isAudioExpander(module)) {
-			if (hasAudioExpander())
+			if (audio_exp_module_id > -1)
 				WARN("Multiple Audio Expanders found: only one will be used\n");
 
-			audio_exp.module_id = module->id;
+			audio_exp_module_id = module->id;
 		}
-	}
 
+		if (ModuleDirectory::isButtonExpander(module)) {
+			if (button_exps.size() >= MaxButtonExpanders)
+				WARN("Found more than %zu Button Expanders\n", MaxButtonExpanders);
+
+			button_exps.push_back(ButtonExp{.module_id = module->id});
+		}
+    }
+    
 	int64_t getAudioExpanderId() const {
-		return audio_exp.module_id;
+		return audio_exp_module_id;
 	}
 
-	bool hasAudioExpander() {
-		return audio_exp.module_id > -1;
-	}
+	
+
 
 	// returns true if cable is connected to an Expander module
 	// that we don't know about (e.g. patch contains more than the max
-	// number of a cetain type of expander)
+	// number of a certain type of expander)
 	bool isUnknownExpanderCable(rack::Cable *cable) {
 		if (!cable)
 			return true;
 
 		if (ModuleDirectory::isAudioExpander(cable->inputModule)) {
-			return (cable->inputModule->getId() != audio_exp.module_id);
+			return (cable->inputModule->getId() != audio_exp_module_id);
 		}
 		if (ModuleDirectory::isAudioExpander(cable->outputModule)) {
-			return (cable->outputModule->getId() != audio_exp.module_id);
+			return (cable->outputModule->getId() != audio_exp_module_id);
 		}
 
 		return false;
 	}
 
 	bool isKnownJackExpander(int64_t module_id) {
-		return module_id == audio_exp.module_id;
+		return module_id == audio_exp_module_id;
 	}
 
 	// Adjusts the cable jack ID from the expander's internal ID
