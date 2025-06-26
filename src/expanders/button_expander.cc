606d276d32e48799be3f2437e0626d565912a750
Dan Green <danngreen1@gmail.com>
2025-06-25 21:37:07 -0700

    Display expander address on faceplace and change it with context menu

--- src/expanders/button_expander.cc
+++ src/expanders/button_expander.cc
@@ -10,6 +10,8 @@ namespace MetaModule
 ButtonExpanderModule::ButtonExpanderModule()
 	: MetaModuleHubBase(8) {
 
+	using Info = MMButtonExpanderInfo;
+
 	// Register with VCV the number of elements of each type
 	auto cnt = ElementCount::count<Info>();
 	config(cnt.num_params, cnt.num_inputs, cnt.num_outputs, cnt.num_lights);
@@ -25,19 +27,46 @@ void ButtonExpanderModule::process(const ProcessArgs &args) {
 	processMaps();
 }
 
-ButtonExpanderWidget::ButtonExpanderWidget(MetaModuleHubBase *module)
-	: MetaModuleHubWidget{module} {
+ButtonExpanderWidget::ButtonExpanderWidget(ButtonExpanderModule *module)
+	: MetaModuleHubWidget{module}
+	, id_label{nullptr}
+	, buttonExpModule{module} {
+
 	using namespace rack;
+	using Info = MMButtonExpanderInfo;
 
 	setModule(module);
 	setPanel(APP->window->loadSvg(rack::asset::plugin(pluginInstance, std::string{Info::svg_filename})));
 	addChild(createWidget<ScrewBlack>(rack::math::Vec(RACK_GRID_WIDTH, 0)));
 	addChild(createWidget<ScrewBlack>(rack::math::Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
+	id_label = createWidget<rack::Label>(rack::math::Vec(55, RACK_GRID_HEIGHT - 30));
+	id_label->color = nvgRGB(0xFF, 0x8F, 0x8F);
+	id_label->text = "";
+	id_label->fontSize = 11;
+	addChild(id_label);
+
 	HubWidgetCreator<Info> creator(this, module);
 	for (auto &element : Info::Elements) {
 		std::visit([&creator](auto &el) { creator.create(el); }, element);
 	}
+}
+
+void ButtonExpanderWidget::step() {
+	if (id_label && buttonExpModule)
+		id_label->text = "#" + std::to_string(buttonExpModule->buttonExpanderId + 1);
+
+	MetaModuleHubWidget::step();
+}
+
+void ButtonExpanderWidget::appendContextMenu(rack::Menu *menu) {
+	using namespace rack;
+
+	menu->addChild(new MenuSeparator());
+	menu->addChild(createIndexPtrSubmenuItem("Address:", {"1", "2", "3", "4"}, &buttonExpModule->buttonExpanderId));
+	menu->addChild(new MenuSeparator());
+
+	MetaModuleHubWidget::appendContextMenu(menu);
 }
 
 } // namespace MetaModule
--- src/expanders/button_expander.hh
+++ src/expanders/button_expander.hh
@@ -2,20 +2,29 @@
 #include "CoreModules/hub/MMButtonExpander_info.hh"
 #include "hub/hub_module.hh"
 #include "hub/hub_module_widget.hh"
+#include "patch/mapping_ids.hh"
 
 namespace MetaModule
 {
 
 struct ButtonExpanderModule : MetaModuleHubBase {
-	using Info = MMButtonExpanderInfo;
 	ButtonExpanderModule();
+
 	void process(const ProcessArgs &args) override;
+
+	// Expander Id (0 - 3)
+	unsigned buttonExpanderId = 0;
 };
 
 struct ButtonExpanderWidget : MetaModuleHubWidget {
-	using Info = ButtonExpanderModule::Info;
+	ButtonExpanderWidget(ButtonExpanderModule *module);
 
-	ButtonExpanderWidget(MetaModuleHubBase *module);
+	void step() override;
+	void appendContextMenu(rack::Menu *menu) override;
+
+private:
+	rack::Label *id_label = nullptr;
+	ButtonExpanderModule *buttonExpModule = nullptr;
 };
 
 } // namespace MetaModule
